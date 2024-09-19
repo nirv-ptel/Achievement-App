16b8c6270de980f5a5580a52a8d2bc06d280e5b6
Yash Marakana <yash.radicalloop@gmail.com>
2024-09-19 17:33:12 +0530

    fix: edit icon set

--- frontend/src/components/generate-archivement/GenerateArchivement.tsx
+++ frontend/src/components/generate-archivement/GenerateArchivement.tsx
@@ -291,18 +291,28 @@ const GenerateAchivement = () => {
 
           {/* Show the cropped image if available */}
           {croppedImageUrl && (
-            <div className="flex flex-col items-center mb-4">
+            <div className="flex flex-col items-center mb-4 relative">
               <img
                 src={croppedImageUrl}
                 alt="Cropped Image"
-                className="w-48 h-auto"
+                className="w-full h-auto"
               />
               <button
                 type="button"
                 onClick={handleEdit}
-                className="btn btn-secondary py-2 px-4 bg-gray-500 text-white rounded hover:bg-gray-600 mt-4"
+                className="btn btn-secondary py-1 px-1 bg-gray-500 text-white hover:bg-gray-600 absolute top-[-5px] right-[-8px] rounded-full"
               >
-                Edit
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="16px"
+                  height="16px"
+                >
+                  <path
+                    fill="white"
+                    d="M 18.414062 2 C 18.158062 2 17.902031 2.0979687 17.707031 2.2929688 L 15.707031 4.2929688 L 14.292969 5.7070312 L 3 17 L 3 21 L 7 21 L 21.707031 6.2929688 C 22.098031 5.9019687 22.098031 5.2689063 21.707031 4.8789062 L 19.121094 2.2929688 C 18.926094 2.0979687 18.670063 2 18.414062 2 z M 18.414062 4.4140625 L 19.585938 5.5859375 L 18.292969 6.8789062 L 17.121094 5.7070312 L 18.414062 4.4140625 z M 15.707031 7.1210938 L 16.878906 8.2929688 L 6.171875 19 L 5 19 L 5 17.828125 L 15.707031 7.1210938 z"
+                  />
+                </svg>
               </button>
             </div>
           )}
